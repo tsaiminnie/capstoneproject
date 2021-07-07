32e97e5e01f286ec461b24bb4c56d08e27203c68
yfeng04 <yfeng59@my.bcit.ca>
2021-07-06 19:53:08 -0700

    edited for styling

--- front-page.php
+++ front-page.php
@@ -160,7 +160,7 @@ get_header();
 				$query = new WP_Query( $args );
 				if ( $query -> have_posts() ){
 					?>
-					<section class="testimonial"><h2>Testimonial</h2>
+					<section class="testimonial"><h2>What Our Customers Say</h2>
 					<?php
 					while ($query-> have_posts() ){
 						?>
@@ -191,7 +191,7 @@ get_header();
 				$query = new WP_Query( $args );
 				if ( $query -> have_posts() ){
 					?>
-					<section class="social"><h2>Social</h2>
+					<section class="social"><h2>As Featured in</h2>
 					<?php
 					while ($query-> have_posts() ){
 						$query -> the_post();
